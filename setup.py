a59812d174b728c9cc23caf40c11902bf0c32fde
Tom van Neerijnen <tom@tomvn.com>
2017-01-23 21:20:02 +0000

    bump to version 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aioldsplice',
-    version='0.0.1',
+    version='0.0.2',
     description='asyncio splicing socket proxy',
     url='http://github.com/drie/aioldsplice',
     author='Tom van Neerijnen',
